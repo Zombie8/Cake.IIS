c28fe17de3cc9eabceefda35656b5b161a2f9032
Zachary Snyder <zach.snyder@cwtinteractive.com>
2016-10-20 10:38:49 -0400

    * hoisted class constructor and private/public methods to top of file. * changed get accessor methods on static methods to user c# method expressions instead.

--- src/IIS/Types/BindingProtocol.cs
+++ src/IIS/Types/BindingProtocol.cs
@@ -4,78 +4,50 @@ namespace Cake.IIS
 {
     public class BindingProtocol
     {
-        /// <value>
-        /// <see cref="BindingProtocol"/> for <c>ftp</c> IIS binding type.
-        /// </value>
-        public static BindingProtocol Ftp
-        {
-            get { return new BindingProtocol(Uri.UriSchemeFtp); }
-        }
-
-        /// <value>
-        /// <see cref="BindingProtocol"/> for <c>http</c> IIS binding type.
-        /// </value>
-        public static BindingProtocol Http
-        {
-            get { return new BindingProtocol(Uri.UriSchemeHttp); }
-        }
-
-        /// <value>
-        /// <see cref="BindingProtocol"/> for <c>https</c> IIS binding type.
-        /// </value>
-        public static BindingProtocol Https
-        {
-            get { return new BindingProtocol(Uri.UriSchemeHttps); }
-        }
-
-        /// <value>
-        /// <see cref="BindingProtocol"/> for <c>net.tcp</c> IIS binding type.
-        /// </value>
-        public static BindingProtocol NetTcp
-        {
-            get { return new BindingProtocol(Uri.UriSchemeNetTcp); }
-        }
-
-        /// <value>
-        /// <see cref="BindingProtocol"/> for <c>net.pipe</c> IIS binding type.
-        /// </value>
-        public static BindingProtocol NetPipe
-        {
-            get { return new BindingProtocol(Uri.UriSchemeNetPipe); }
-        }
-
-        /// <value>
-        /// <see cref="BindingProtocol"/> for <c>net.msmq</c> IIS binding type.
-        /// </value>
-        public static BindingProtocol NetMsmq
-        {
-            get { return new BindingProtocol("net.msmq"); }
-        }
-
-        /// <summary>
-        /// <see cref="BindingProtocol"/> for <c>msmq.formatname</c> IIS binding type.
-        /// </summary>
-        public static BindingProtocol MsmqFormatName
-        {
-            get { return new BindingProtocol("msmq.formatname"); }
-        }
-
-
-
         private BindingProtocol(string name)
         {
             this.Name = name;
         }
 
-
-
         private string Name { get; set; }
 
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString() => this.Name;
 
+        /// <value>
+        /// <see cref="BindingProtocol"/> for <c>ftp</c> IIS binding type.
+        /// </value>
+        public static BindingProtocol Ftp => new BindingProtocol(Uri.UriSchemeFtp);
 
-        public override string ToString()
-        {
-            return this.Name;
-        }
+        /// <value>
+        /// <see cref="BindingProtocol"/> for <c>http</c> IIS binding type.
+        /// </value>
+        public static BindingProtocol Http => new BindingProtocol(Uri.UriSchemeHttp);
+
+        /// <value>
+        /// <see cref="BindingProtocol"/> for <c>https</c> IIS binding type.
+        /// </value>
+        public static BindingProtocol Https => new BindingProtocol(Uri.UriSchemeHttps);
+
+        /// <value>
+        /// <see cref="BindingProtocol"/> for <c>net.tcp</c> IIS binding type.
+        /// </value>
+        public static BindingProtocol NetTcp => new BindingProtocol(Uri.UriSchemeNetTcp);
+
+        /// <value>
+        /// <see cref="BindingProtocol"/> for <c>net.pipe</c> IIS binding type.
+        /// </value>
+        public static BindingProtocol NetPipe => new BindingProtocol(Uri.UriSchemeNetPipe);
+
+        /// <value>
+        /// <see cref="BindingProtocol"/> for <c>net.msmq</c> IIS binding type.
+        /// </value>
+        public static BindingProtocol NetMsmq => new BindingProtocol("net.msmq");
+
+        /// <summary>
+        /// <see cref="BindingProtocol"/> for <c>msmq.formatname</c> IIS binding type.
+        /// </summary>
+        public static BindingProtocol MsmqFormatName => new BindingProtocol("msmq.formatname");
     }
 }
